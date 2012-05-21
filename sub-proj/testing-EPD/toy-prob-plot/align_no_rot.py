dfb1f01026947d684809df988fdd01c1ad0c69d5
Stan Bileschi <sbileschi@dataxu.com>
2012-05-21 02:12:01 -0400

    added illustration to forces

--- sub-proj/testing-EPD/toy-prob-plot/align_no_rot.py
+++ sub-proj/testing-EPD/toy-prob-plot/align_no_rot.py
@@ -3,7 +3,31 @@ import math, sys, getopt
 from random import random
 import matplotlib.pyplot as plt
 
-#say points1 is fixed, points2 translates, and rotates
+def illustrate_points(points1, points2):
+	plt.figure(0)
+	plt.clf()
+	plt.plot([x for [x,y] in points1], [y for [x,y] in points1], 'ro')
+	plt.plot([x for [x,y] in points2], [y for [x,y] in points2], 'bx')
+	plt.axis([-7, 7, -5, 5])
+	plt.grid(True)
+	plt.title('this is a title')
+	plt.xlabel('this is x label')
+	plt.ylabel('this is y label')
+	plt.show()
+	plt.pause(0.1)
+
+def illustrate_forces(force_list):
+	"force_list is like [(f_x1, f_y1), (f_x2, f_y2), ... ]"
+	x_force = [x for (x,y) in force_list]
+	y_force = [y for (x,y) in force_list]
+	plt.figure(1)
+	plt.clf()
+	plt.subplot(2, 1, 1)
+	plt.bar(range(len(x_force)), x_force)
+	plt.subplot(2, 1, 2)
+	plt.bar(range(len(y_force)), y_force)
+	plt.show()
+	plt.pause(0.05)
 
 class PointAligner(object):
 
@@ -12,34 +36,30 @@ class PointAligner(object):
 		pass
 
 	@staticmethod
-	def align(points1, points2, iterations):
-		print "points1 = " + `points1` + " and points2 = " + `points2` + ".\n"\
-				"aligning with " + `iterations` + "\iterations."
+	def align(points1, points2, iterations, illustrate_on = False, verbose_on = True):
+		if(verbose_on):
+			print "points1 = " + `points1` + " and points2 = " + `points2` + ".\n"\
+					"aligning with " + `iterations` + "\iterations."
 
 		for i in range(iterations):
-			print "iteration = " + `i+1`
+			# Draw current state of alignment
+			if (illustrate_on):
+				illustrate_points(points1 = points1, points2 = points2)
+			if (verbose_on):
+				print "iteration = " + `i+1`
 
-			alf = PointAligner.aggregateForce(points1, points2)
+			alf = PointAligner.aggregateForce(points1, points2, illustrate_on = False)
 			unitAlf = PointAligner.unitVector(alf)
-			print "unitalf  = " + `unitAlf`
+			if(verbose_on):
+				print "unitalf  = " + `unitAlf`
 			#print "alf = " + `alf`
 			for j in range(len(points2)):
 				points2[j] = [points2[j][0]+unitAlf[0]*.1, points2[j][1]+unitAlf[1]*.1]
 			
-			print "------------------------------------------------------\npoints2 = " + `points2`
-			print
+			if(verbose_on):
+				print "------------------------------------------------------\npoints2 = " + `points2`
+				print
 
-			# Draw current state of alignment
-			plt.clf()
-			plt.plot([x for [x,y] in points1], [y for [x,y] in points1], 'ro')
-			plt.plot([x for [x,y] in points2], [y for [x,y] in points2], 'bx')
-			plt.axis([-7, 7, -5, 5])
-			plt.grid(True)
-			plt.title('this is a title')
-			plt.xlabel('this is x label')
-			plt.ylabel('this is y label')
-			plt.show()
-			plt.pause(.1)
 
 
 
@@ -52,11 +72,10 @@ class PointAligner(object):
 	#linear force exterted between two points
 	#current model is 1/sqrt
 	@staticmethod
-	def force(x1, y1, x2, y2):
-		r = PointAligner.distance(x1, y1, x2, y2)
-		if r <= .10:
-			return 0
-		return 1/pow(r,.5)
+	def force_mag(d):
+		if d <= .10:
+			return (10 * d) * 0.316 # linear drop off within .1
+		return 1/pow(d, .5)
 
 	@staticmethod
 	def length(x, y):
@@ -68,19 +87,28 @@ class PointAligner(object):
 		d = PointAligner.distance(0,0,v[0],v[1])
 		if d==0:
 			return [0,0]
-		return [v[0]/d,v[1]/d]
+		return [v[0]/d, v[1]/d]
+
+	@staticmethod
+	def force_on_one_point(x1, y1, points2):
+		force_x = 0
+		force_y = 0
+		for (x2,y2) in points2:
+			d = PointAligner.distance(x1, y1, x2, y2)
+			f = PointAligner.force_mag(d)
+			directionVec = PointAligner.unitVector([x1-x2,y1-y2])
+			force_x += directionVec[0]*f
+			force_y += directionVec[1]*f
+		return (force_x, force_y)
 
 	#takes all linear forces between all sets of points, and adds them
 	@staticmethod
-	def aggregateForce(points1, points2):
+	def aggregateForce(points1, points2, illustrate_on = False):
 		forces = []
 		for (x1,y1) in points1:
-			for (x2,y2) in points2:
-				r = PointAligner.distance(x1,y1,x2,y2)
-				f = PointAligner.force(x1,y1,x2,y2)
-				directionVec = PointAligner.unitVector([x1-x2,y1-y2])
-				forces.append( [ directionVec[0]*f , directionVec[1]*f ] )
-
+			forces.append(PointAligner.force_on_one_point(x1, y1, points2))
+		if illustrate_on:
+			illustrate_forces(forces)
 		aggregate=[0,0]
 		for (fx,fy) in forces:
 			aggregate[0]+=fx
@@ -103,11 +131,14 @@ class PointAligner(object):
 
 
 if __name__ == "__main__":
-	err_x = random() * 3
-	err_y = random() * 3
+	from time import time
+	time_start = time()
+	err_x = random() * 1
+	err_y = random() * 1
 	points1 = []
 	points2 = []
-	for i in range(1, 2000):
+	n_pts = 15;
+	for i in range(1, n_pts):
 		x = random()
 		y = random()
 		points1.append([x, y])
@@ -116,6 +147,9 @@ if __name__ == "__main__":
 	PointAligner.align(
 			points1 = points1, 
 			points2 = points2,
-			iterations = 20)
+			iterations = 20,
+			illustrate_on = True,
+			verbose_on = False)
 
+	print "that took %f seconds" % (time() - time_start)
 
